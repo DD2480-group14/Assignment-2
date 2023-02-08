9f01ec138afecaec5ce1ad5f8c34f82f27d82ccf
Aleksey Veresov <aleksey@veresov.pro>
2023-02-08 23:39:46 +0100

    Adapt Webview

--- src/main/java/assignment/Webview.java
+++ src/main/java/assignment/Webview.java
@@ -1,4 +1,3 @@
-
 package assignment;
 
 import java.io.IOException;
@@ -14,25 +13,23 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-
 public class Webview extends AbstractHandler {
-
+    private JSONArray json;
+    private PrintWriter res;
+    private String header = "<html><head><title>Awesome CI</title><style>"
+                    + ".success{color:white;padding:8px;margin:8px;background-color:#3fb950;text-decoration:none;display:inline-block}"
+                    + ".failure{color:white;padding:8px;margin:8px;background-color:#f85149;text-decoration:none;display:inline-block}"
+                    + "</style></head><body>";
+    private String footer = "</body></html>";
 
     @Override
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
                        HttpServletResponse response) throws IOException, ServletException {
-
-        // Load JSON variables
-
-
-        JSONArray jsonBuilds;
-        String[][] builds;
-
         try {
             String content = new String(Files.readAllBytes(Paths.get("builds.json")));
-            jsonBuilds = new JSONArray(content);
+            json = new JSONArray(content);
         } catch (IOException e) {
             System.out.println("Unable to read and parse JSON from build.json. IO");
             return;
@@ -40,69 +37,54 @@ public class Webview extends AbstractHandler {
             System.out.println("Unable to read and parse JSON from build.json. JSON");
             return;
         }
+
+        response.setContentType("text/html; charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        res = response.getWriter();
+        res.print(header);
+
         try {
-            builds = new String[jsonBuilds.length()][4]; // assuming it
-            for (int i = 0; i != jsonBuilds.length(); ++i) {
-                JSONObject build = jsonBuilds.getJSONObject(i);
-                builds[i][0] = build.getString("id");
-                builds[i][1] = build.getString("origin");
-                builds[i][2] = build.getString("status");
-                builds[i][3] = build.getString("log");
+            String[] url = target.split("/");
+            if (target.equals("/")) {
+                serveList(baseRequest, request, response);
+            } else if (url.length == 2) {
+                serveBuild(Integer.parseInt(url[1]), baseRequest, request, response);
+            } else {
+                return;
             }
         } catch (JSONException e) {
-            System.out.println("Unable to load POINTS from builds.json.");
-
             return;
         }
-        // Declare response encoding and types
-        response.setContentType("text/html; charset=utf-8");
 
-        // Declare response status code
-        response.setStatus(HttpServletResponse.SC_OK);
-        PrintWriter res = response.getWriter();
-        String html = "<!DOCTYPE html>" + "\n<html>" + "\n<head>" + "\n<style>"
-                        + "\n.center {text-align: center;}"
-                        + "\n.pagination {display: inline-block;}"
-                        + "\n.pagination a {color: black;float: left;padding: 8px 16px;text-decoration: none;}"
-                        + "\n.pagination a.active {background-color: paleturquoise;color: plum;border: 1px solid paleturquoise;}"
-                        + "\n.pagination a:hover:not(.active) { background-color: lightgreen;}"
-                        + "\n</style>" + "\n<h1>Builds and Requests</h1>" + "\n</head>"
-                        + "\n <body>";
-        res.println(html);
-        // Making a build
-
-        // Making a box and filling it in with build information
-
-        for (int i = 0; i < builds.length; i++) {
-            res.println("\n<br>" + "\n<label for=\"freeform\">Build " + (i + 1) + "</label>"
-                            + "\n<br>"
-                            + "\n<textarea id=\"freeform\" name=\"freeform\" rows=\"5\" cols=\"110\">"
-                            + "\nid: " + builds[i][0] + "\norigin: " + builds[i][1] + "\nstatus: "
-                            + builds[i][2] + "\nlog: " + builds[i][3] + "\n</textarea>" + "\n<br>");
-        }
-
-
-        // pagination
-        /*
-         * int NumberofPaginations = Math.ceil(builds.length / 10);
-         * res.println("\n<h2>More Pages</h2> " + "\n<div class =\"pagination\">" +
-         * "\n<div class =\"pagination\"><a href = \"#\">&laquo;</a>"); for (int j = 0; j <
-         * NumberofPaginations; j++) { res.println("\n<a href=\"#\">" + j + 1 + "</a>"); }
-         * res.println("\n<a href=\"#\">&raquo;</a>"); res.println("\n<a href=\"#\">&raquo;</a>" +
-         * "\n</div>" + "\n</body>" + "\n</html>");
-         * 
-         */
+        res.print(footer);
         baseRequest.setHandled(true);
-
-        // Using requestcount we can decide number of paginations
-
-
-
     }
 
+    void serveList(Request baseRequest,
+                   HttpServletRequest request,
+                   HttpServletResponse response) throws JSONException {
+        res.print("<h1>Builds:</h1>");
+        for (int i = json.length() - 1; i != -1; --i) {
+            res.print("<a class=\"");
+            JSONObject build = json.getJSONObject(i);
+            if (build.getEnum(Builder.Result.class, "status") == Builder.Result.Success) {
+                res.print("success");
+            } else {
+                res.print("failure");
+            }
+            res.print("\" href=\"/" + i + "/\">");
+            res.print("Build #" + i + "</a><br/>");
+        }
+    }
 
-
+    void serveBuild(int buildId,
+                    Request baseRequest,
+                    HttpServletRequest request,
+                    HttpServletResponse response) {
+        res.print("<a href=\"/\">go back</a>");
+        res.print("<h1>Build #" + buildId + "</h1>");
+        JSONObject build = json.getJSONObject(buildId);
+        res.print("<a href=\"" + build.getString("origin") + "\">go to source</a>");
+        res.print("<pre>" + build.getString("log") + "</pre>");
+    }
 }
-
-// res.println(style)
-// res.println
